d68c0f7dcb41edfe1bd9e22b2421ba346077c632
Abel Moreno <moreno.abel@gmail.com>
2026-06-22 13:33:22 -0500

    Implementing payments on admin

--- src/app/Enums/SubscriptionPlan.php
+++ src/app/Enums/SubscriptionPlan.php
@@ -5,6 +5,7 @@ namespace App\Enums;
 enum SubscriptionPlan: string
 {
     case Starter = 'starter';
+    case StarterAnnual = 'starter_annual';
     case Pro = 'pro';
     case Business = 'business';
 }
